71be05f275de14257d82c5e13948389cab0ecf8e
Tuan Razaqhi <bdkatapang@gmail.com>
2026-02-21 00:27:47 +0700

    Create code.js

---
+++ code.js
@@ -0,0 +1,13 @@
+var _0x12ab = ["LIAT AJA PORTFOLIONYA AJG"];
+console.log(_0x12ab[0]);
+
+function greet(name) {
+    alert("HALO ANAK HARAM, " + name + "!");
+}
+
+var btn = document.querySelector("button");
+if (btn) {
+    btn.addEventListener("click", function() {
+        greet("PECUNDANG");
+    });
+}
